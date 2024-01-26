9c70843755109447c69584cb310afaa8b66631be
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-26 05:38:17 -0300

    aula 4 e aula 5

---
+++ aula4 - comentários/comentarios.js
@@ -0,0 +1,13 @@
+// Este é um comentário. Ele não vai aparecer quando mandarmos executar nosso código
+
+console.log('Olá mundo.') // Aqui tem outro comentário
+console.log('Linha 1')
+console.log('Linha 2')
+/*
+Este é um comentário de bloco
+Ele serve comentarmos mais de 1 linha de código
+*/
+console.log('Linha 3')
+console.log('Linha 4')
+// Vai ser ignorado
+
