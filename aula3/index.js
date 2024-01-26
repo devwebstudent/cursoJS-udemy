97b5ba5c818fc5fe399bb09b77bfbac2b9f5c270
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-26 04:50:04 -0300

    curso de JavaScript - Udemy

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+/* O ponto e vírgula no JavaScript é opcional */
+console.log('Olá mundo'); //Tipo de dado: STRING (texto)
+console.log("Olá mundo"); //Tipo de dado: STRING (texto)
+console.log(`Olá mundo`); //Tipo de dado: STRING (texto)
+
+
+// Também podemos exibir números
+console.log(1234); //Número inteiro - Tipo de dado: NUMBER (número)
+console.log(15.85); //Número real (ponto flutuante)
+console.log(1234, 15.85, 'Luiz Otávio');
