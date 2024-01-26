f576750fff970692a7496ea896d2c49303c176e4
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-26 05:02:56 -0300

    update resolvendo exercício

--- aula3/exercícios/index.js
+++ aula3/exercícios/index.js
@@ -1,1 +1,2 @@
 console.log('Meu nome é "Júnior". Estou aprendendo JavaScript às', 5, 'da manhã.');
+console.log('Este', 'É', 'Um', 1000); //Colocamos os valores a serem exibidos separados por vírgula
