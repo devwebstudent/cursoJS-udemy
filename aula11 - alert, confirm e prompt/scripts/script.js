897c348c4af86d217ce6589d0cf4eeabe5c48929
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-27 19:19:11 -0300

    aula 11 - alert, confirm e prompt

---
+++ aula11 - alert, confirm e prompt/scripts/script.js
@@ -0,0 +1,4 @@
+// Quando chamamos () estamos chamando uma função, e uma função executa uma ação.
+window.alert('Olá, mundo'); // Não temos alerta no node, por isso estamos trabalhando no navegador.
+window.confirm('Tem certeza que quer apagar essa mensagem?') // Serve para confirmar coisas do usuário.
+window.prompt('Digite o seu nome.')
