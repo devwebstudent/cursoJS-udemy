52cf5e9fb6e8b528cecc71a4d46ffc454382c57d
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-27 04:36:58 -0300

    aula 9 - let vs var - primeira diferença

---
+++ aula9 - let vs var - primeira diferença/index.js
@@ -0,0 +1,29 @@
+var nome = 'Luiz';
+var nome = 'Otávio';
+console.log(nome);
+
+let num = 13
+let num = 14
+console.log(num)
+
+nome2 = 'Olá mundo' // Sem utilizar nenhuma palavra (var, let, const), posso declarar uma variável - NÃO FAÇA ISSO!! -> Isso vai criar uma variável global no seu sistema geral e pode afetar outras variáveis do seu sistema
+console.log(nome2)
+
+/*
+A primeira diferença entre VAR e LET é:
+
+Com VAR -> posso redeclarar uma variável, como podemos ver nas linhas 1 e 2 do nosso código;
+Com o LET -> não é possível redeclarar uma variável, como podemos ver nas linhas 5 e 6. Isso irá gerar um erro ao executarmos nosso código, dizendo que nosso LET já foi declarado.
+
+
+O LET surgiu para corrigir esse problema de redeclararmos uma variável.
+*/
+
+
+
+
+/* IMPORTANTE!!!
+
+NÃO CRIE VARIÁVEIS SEM LET OU CONST!!!!!!!!!!!!!!!! 
+
+*/
