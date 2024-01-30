4cfd9d3abd781ac287c725600ed64cf229c692ce
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-01-30 16:57:11 -0300

    apenas para testar js

--- APENAS PARA TESTAR JAVASCRIPT/teste.js
+++ APENAS PARA TESTAR JAVASCRIPT/teste.js
@@ -1,5 +1,18 @@
-let num = 15;
+let num1 = 15;
 let num2 = 12.85;
+let num3 = 84.57455005587498489;
+let nome = 'José';
 
-console.log(Number.isNaN(num))
-console.log(Math.round(Math.random() * 100))
+console.log(num1 + num2)
+console.log(typeof(nome))
+console.log(num3.toFixed(2))
+console.log(Math.floor(num3))
+console.log(Math.ceil(num3))
+console.log(Math.round(num3))
+console.log(Math.min(num1, num2, num3))
+console.log(Math.max(num1, num2, num3))
+console.log(Math.random())
+console.log(Math.round(Math.random() * 100)) // Arredondando o número aleatório gerado
+
+console.log(Number.isInteger(num1))
+console.log(Number.isInteger(num2))
