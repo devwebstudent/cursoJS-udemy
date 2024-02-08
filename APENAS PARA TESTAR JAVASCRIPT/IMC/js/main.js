b62fec7535b30a4b8c14eb93369266af1ace5693
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-02-08 05:39:56 -0300

    update calculadora imc

--- APENAS PARA TESTAR JAVASCRIPT/IMC/js/main.js
+++ APENAS PARA TESTAR JAVASCRIPT/IMC/js/main.js
@@ -4,13 +4,20 @@ let resultado = document.querySelector('#resultado');
 function calcularIMC(evento) {
     evento.preventDefault();
     let txtpeso = document.querySelector('#peso');
-    let txtaltura = document.querySelector('#altura');
+    let txtaltura = document.querySelector('#altura');    
+    
 
-    let peso = Number(txtpeso.value)
-    let altura = Number(txtaltura.value)
-    let imc = peso / (altura * altura);
-    resultado.innerHTML = '';
-    resultado.innerHTML = `<p style="text-align: center;">Seu IMC é de <strong style="color:green;">${imc.toFixed(2)}</strong></p>`;
+    if (txtpeso.value.length === 0 || txtaltura.value.length === 0) {
+        alert('[ERRO]: Peso ou altura inválidos. Tente novamente!')
+    } else {
+        let peso = Number(txtpeso.value)
+        let altura = Number(txtaltura.value)
+        let imc = peso / (altura * altura);
+
+        resultado.innerHTML = '';
+        resultado.innerHTML = `<p style="text-align: center;">Seu IMC é de <strong style="color:green;">${imc.toFixed(2)}</strong></p>`;        
+    }
 }
+    
 
 let calc = form.addEventListener('submit', calcularIMC)
