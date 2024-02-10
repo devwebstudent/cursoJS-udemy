5a7f39200281d51624471eb790a70904be1089c8
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-02-10 20:48:27 -0300

    update

--- Seção 3/aula032 - atribuição via desestruturação (arrays)/index3.js
+++ Seção 3/aula032 - atribuição via desestruturação (arrays)/index3.js
@@ -4,6 +4,6 @@
 
 // Índices       0     1     2     3     4     5     6     7     8
 const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
-const [um, , tres, , cinco, , sete] = numeros
+const [um, ,tres, ,cinco, ,sete] = numeros
 
 console.log(um, tres, cinco, sete) // Resultado: 1000 3000 5000 7000
