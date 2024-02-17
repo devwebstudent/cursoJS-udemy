d6d59e4534b1b9081efa1c01b5bed8933b7d551b
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-02-17 00:35:51 -0300

    revisando

--- Seção 3/aula037 - estrutura de repetição - FOR OF/index.js
+++ Seção 3/aula037 - estrutura de repetição - FOR OF/index.js
@@ -3,7 +3,7 @@
 
 //            012345678910
 const nome = 'Luiz Otávio';
-console.log(nome.length)
+console.log(nome.length) // 11 elementos
 
 // Iterando essa string utilizando o FOR clássico:
 for (let i = 0; i < nome.length; i++) { // Isso vai acessar cada letra da nossa string
