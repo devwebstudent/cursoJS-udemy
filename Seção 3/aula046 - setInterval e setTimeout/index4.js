0846355a4065ff08b841953b18cb9930529121c4
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-02-20 11:21:52 -0300

    Atualizar o index4.js

--- Seção 3/aula046 - setInterval e setTimeout/index4.js
+++ Seção 3/aula046 - setInterval e setTimeout/index4.js
@@ -1,7 +1,7 @@
 // RESUMINDO (ChatGPT): 
 
-// setTimeout: Esta função é utilizada para executar uma função depois de um determinado tempo (em milissegundos).
+// setTimeout: Função que executa uma função depois de um determinado tempo (em milissegundos).
 
-// setInterval: Já esta função é utilizada para executar uma função repetidamente em determinado intervalo de tempo. (novamente, em milissegundos).
+// setInterval: Executa uma função repetidamente em determinado intervalo de tempo. (novamente, em milissegundos).
 
-// clearInterval: é usado para parar a execução de uma função que está sendo chamada repetidamente em intervalos definidos pelo setInterval().
+// clearInterval: é usado para parar a execução de uma função que está sendo chamada pelo setInterval().
