fead4c283bed1e3a9a5691fd6776bbbc29947da5
devwebstudent <142200863+devwebstudent@users.noreply.github.com>
2024-04-08 23:33:41 -0300

    relogio

---
+++ APENAS PARA TESTAR JAVASCRIPT/RELOGIO/assets/js/main.js
@@ -0,0 +1,14 @@
+(() => {
+    let container = document.querySelector('.container')
+    let h1 = document.querySelector('#timer')
+
+    function relogio() {
+        let hora = new Date()
+        return hora.toLocaleTimeString('pt-br')        
+    }
+
+    setInterval(() => {        
+        h1.innerHTML = `${relogio()}`
+    }, 1000)
+}
+)()
